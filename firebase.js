dd73c7817e4cb5e0f0196e5f1f9743db427a47ad
emi <a01368665@tec.mx>
2024-06-07 13:26:42 -0600

    nuevos cambios

--- firebase.js
+++ firebase.js
@@ -1,11 +1,24 @@
+// Load environment variables from .env file
+require('dotenv').config(); 
 const admin = require('firebase-admin');
 const firebaseConfig = require('./firebaseconfig.json');
 
 
 
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseConfig),
-  databaseURL: firebaseConfig.databaseURL,
+  credential: admin.credential.cert({
+    type: process.env.TYPE,
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escape characters
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  }),
+  databaseURL: process.env.DATABASE_URL,
 });
 
 const db = admin.database();
